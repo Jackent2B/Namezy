48385a869d0810cd0407d8da1964355e47e09b2e
Jayant Khandelwal <jayant.khandelwal7@gmail.com>
2021-06-15 17:41:59 +0530

    Namezy!

---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import './Searchbar.css';
+
+function SearchBar() {
+  const [search, setSearch] = useState('');
+  const [words, setWords] = useState([]);
+  const name = require('@rstacruz/startup-name-generator');
+  useEffect(() => {
+    //console.log(name(search).slice(0, 12));
+    if (name(search).length >= 12) {
+      setWords(name(search).slice(0, 12));
+    } else setWords(name(search));
+  }, [search]);
+  return (
+    <div id='searchBox'>
+      <div>
+        <input
+          type='text'
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder='Search Name'
+        />
+      </div>
+      <div>
+        {words.map((word) => (
+          <p>{word}</p>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default SearchBar;
---
+++ src/Components/Title/Title.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Title.css';
+function Title() {
+  return (
+    <div>
+      <h1>NAMEZY!</h1>
+      <img
+        src='https://user-images.githubusercontent.com/26179770/106359099-693e3380-6336-11eb-8069-9e36f25de5ca.png'
+        alt='vector'
+      />
+    </div>
+  );
+}
+
+export default Title;
